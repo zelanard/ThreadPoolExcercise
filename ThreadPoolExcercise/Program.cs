ba5d6148e0937a3dfd768e430eb7a288d1218f30
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2024-05-13 22:52:22 +0200

    Add project files.

---
+++ ThreadPoolExcercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//As you said that we can wait with turning in this project until tomorrow, I have not yet given this a proper go, as I was expecting to do that tomorrow.
+//I can however see that you've set it to be turned in today, so I am making the git project and I'm turning it in.
+//If this is the text you see, I appologize for the confusoin. The code will be here soon.
+
+namespace ThreadPoolExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
