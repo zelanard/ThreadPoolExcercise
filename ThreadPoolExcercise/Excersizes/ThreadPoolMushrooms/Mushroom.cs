6a51e605648f2f3338c778f572c0cd433106f838
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2024-05-14 08:54:39 +0200

    Excersize 3

---
+++ ThreadPoolExcercise/Excersizes/ThreadPoolMushrooms/Mushroom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadPoolExcercise.Excersizes.ThreadPoolMushrooms
+{
+    public class Mushroom
+    {
+        public async void Spore(int seed)
+        {
+            for (int i = 0; i < seed; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Growth);
+            }
+            Console.ReadKey();
+        }
+
+        public void Growth(object obj)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            string spore = $"Spore: {Thread.CurrentThread.ManagedThreadId}";
+            while (sw.ElapsedMilliseconds < 30000)
+            {
+                Console.WriteLine($"{spore} | Growth progress: {sw}");
+            }
+            Console.WriteLine($"{spore} is matured. New mushrooms are now growing from the Mycelium!");
+        }
+    }
+}
--- ThreadPoolExcercise/Program.cs
+++ ThreadPoolExcercise/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using ThreadPoolExcercise.Excersizes.ThreadPoolMushrooms;
 
 //As you said that we can wait with turning in this project until tomorrow, I have not yet given this a proper go, as I was expecting to do that tomorrow.
 //I can however see that you've set it to be turned in today, so I am making the git project and I'm turning it in.
@@ -17,6 +19,8 @@ namespace ThreadPoolExcercise
     {
         static void Main(string[] args)
         {
+            Mushroom mush = new Mushroom();
+            mush.Spore(5);
         }
     }
 }
